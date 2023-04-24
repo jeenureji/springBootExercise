09faca0bd3fd695635b189048e96d110d4f0fcde
jeenureji <jeenureji@gmail.com>
2023-04-24 21:46:34 +0530

    new story

---
+++ src/main/java/com/collage/students/fouryearscollage/FourYearsCollageApplication.java
@@ -0,0 +1,13 @@
+package com.collage.students.fouryearscollage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FourYearsCollageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FourYearsCollageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collage/students/fouryearscollage/FourYearsCollageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collage.students.fouryearscollage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FourYearsCollageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
