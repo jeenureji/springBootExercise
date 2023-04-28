f98e96c50ddece8aa56d521f89cd3a2cdfd8d7ba
jeenureji <jeenureji@gmail.com>
2023-04-29 01:04:12 +0530

    student - exception if id is more then 3 numbers

--- src/main/java/com/collage/students/fouryearscollage/controller/StudentController.java
+++ src/main/java/com/collage/students/fouryearscollage/controller/StudentController.java
@@ -1,14 +1,26 @@
 package com.collage.students.fouryearscollage.controller;
 
 import com.collage.students.fouryearscollage.model.Student;
+import com.collage.students.fouryearscollage.service.InvalidUserIdException;
 import com.collage.students.fouryearscollage.service.StudentService;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.persistence.criteria.CriteriaBuilder;
+import java.time.LocalDate;
 import java.util.List;
 
+@Slf4j
 @RestController
 public class StudentController {
+
+    private static final Logger logger = LoggerFactory.getLogger(StudentController.class);
+
 
     @Autowired
     StudentService studentService;
@@ -21,8 +33,21 @@ public class StudentController {
     public void createStudent(@RequestBody Student student){
         studentService.createStudent(student);
     }
+
+
     @GetMapping("/student/{id}")
-    public Student getStudentById(@PathVariable Integer id){
-        return studentService.getStudentById(id);
+    public ResponseEntity<Student> getStudentById(@PathVariable String id) throws InvalidUserIdException {
+        if(id.length() > 3){
+            throw new InvalidUserIdException("Invalid student id:"+ id);
+        }
+        try {
+            Integer studentId = Integer.parseInt(id);
+            Student student = studentService.getStudentById(studentId);
+            logger.info("student id passed is:"+ studentId);
+            return new ResponseEntity<Student>(student, HttpStatus.ACCEPTED);
+        } catch (NumberFormatException e) {
+            throw new InvalidUserIdException("Invalid student id: " + id + ", date time:" + LocalDate.now());
+        }
     }
+
 }
---
+++ src/main/java/com/collage/students/fouryearscollage/service/InvalidUserIdException.java
@@ -0,0 +1,11 @@
+package com.collage.students.fouryearscollage.service;
+
+public class InvalidUserIdException extends Exception {
+
+    public InvalidUserIdException(String message){
+        super(message);
+    }
+
+
+
+}
